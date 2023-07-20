09fb6e1193ea12cb00cc0041a8108301dc0bc114
XK <47260768+XK9274@users.noreply.github.com>
2023-07-20 23:22:52 +0100

    Start with no keyboard

--- BetterWifi/script/launch.sh
+++ BetterWifi/script/launch.sh
@@ -4,4 +4,4 @@ export sysdir=/mnt/SDCARD/.tmp_update
 export miyoodir=/mnt/SDCARD/miyoo
 export LD_LIBRARY_PATH="/mnt/SDCARD/App/BetterWifi/lib:/lib:/config/lib:$miyoodir/lib:$sysdir/lib:$sysdir/lib/parasyte"
 export PATH="$sysdir/bin:$PATH"
-$sysdir/bin/st -e /mnt/SDCARD/App/BetterWifi/script/launchenv.sh
+$sysdir/bin/st -q -e /mnt/SDCARD/App/BetterWifi/script/launchenv.sh
